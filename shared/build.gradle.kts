7059acaed867a871ad133fcd919f2d095d04d6d5
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-30 20:26:03 +0200

    fix tests

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -51,7 +51,7 @@ kotlin {
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser {
-            testTask { useKarma { useChromiumHeadless() } }
+            testTask { useKarma { useChromeHeadless() } }
             val rootDirPath = project.rootDir.path
             val projectDirPath = project.projectDir.path
             commonWebpackConfig {
