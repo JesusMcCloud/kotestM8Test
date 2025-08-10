19c1371c53979bdc68b90b364b4a16c6dbd14a9d
sksamuel <sam@sksamuel.com>
2025-08-10 04:15:56 -0500

    Updated to M15

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 plugins {
-  // this is necessary to avoid the plugins to be loaded multiple times
-  // in each subproject's classloader
-  alias(libs.plugins.kotlinMultiplatform) apply false
-  alias(libs.plugins.androidLibrary) apply false
-  alias(libs.plugins.kotest)
-  alias(libs.plugins.ksp)
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotest)
+    alias(libs.plugins.ksp)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 
 
 // Blocked by KSP.
+///////// ITS EITHER THIS BLOCK OR DISABLING WAMSWASI //////////////////////////////////////////////////////////////////
 tasks.configureEach {
     val n = name
     if (
@@ -24,14 +25,16 @@ configurations
     .matching { it.name.endsWith("wasmWasiTestCompileClasspath") || it.name.endsWith("wasmWasiTestRuntimeClasspath") }
     .configureEach {
         exclude(
-            group  = "io.kotest",
+            group = "io.kotest",
             module = "kotest-framework-engine-wasm-wasi"
         )
         exclude(
-            group  = "io.kotest",
+            group = "io.kotest",
             module = "kotest-assertions-core-wasm-wasi"
         )
     }
+// END BLOCKED BY SKP //////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 kotlin {
 
@@ -40,7 +43,14 @@ kotlin {
         languageVersion = KotlinVersion.KOTLIN_2_2
     }
 
+    // for android tests, use the `kotestDebugUnitTest` or `kotestReleaseUnitTest` tasks
     androidTarget()
+
+    // native has a bug - test reports aren't written at the module level, but they are at the aggregated root level
+    // note: to run tests for native targets, use the standard gradle task eg linuxX64Test
+    linuxX64()
+    linuxArm64()
+    mingwX64()
 
     macosArm64()
     macosX64()
@@ -56,29 +66,32 @@ kotlin {
     tvosX64()
     tvosArm64()
 
+    // kotlin has no support for tests for androidNative targets, so they'll be skipped at runtime
+    // it will also error saying the compiler is only at 2.0 - I cannot figure out how why since it's set to 2.2
     androidNativeX64()
     androidNativeX86()
     androidNativeArm32()
     androidNativeArm64()
 
-    linuxX64()
-    linuxArm64()
-    mingwX64()
-
+    // to run JVM tests use either the gradle task `jvmTest` (requires engine dep) or the `jvmKotest` task (enhanced Kotest support)
     jvm()
 
+    // for js tests run the `jsKotest` task
     js {
+        outputModuleName = "@kotest/test"
+        version = "1.0.0"
+        binaries.executable()
         nodejs()
-        browser { testTask { useKarma { useChromeHeadless() } }}
+        browser { testTask { useKarma { useChromeHeadless() } } }
     }
 
+    // won't do anything but won't fail either
     @OptIn(ExperimentalWasmDsl::class)
     wasmWasi {
         nodejs()
     }
 
-
-
+    // for wasmJs tests run the `wasmJsKotest` task
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser {
@@ -99,20 +112,33 @@ kotlin {
     }
 
     sourceSets {
+        commonMain {
+            dependencies {
+                api(kotlin("stdlib"))
+            }
+        }
         commonTest {
             dependencies {
                 implementation(libs.kotest.framework)
                 implementation(libs.kotest.assert)
             }
         }
+        // these deps should be added by gradle automatically from common main, but they don't seem to always work
         jsMain {
             dependencies {
                 api(kotlin("stdlib-js"))
             }
         }
+        // these deps should be added by gradle automatically from common main, but they don't seem to always work
         wasmJsMain {
             dependencies {
                 api(kotlin("stdlib-wasm-js"))
+            }
+        }
+        // these deps should be added by gradle automatically from common main, but they don't seem to always work
+        wasmWasiMain {
+            dependencies {
+                api(kotlin("stdlib-wasm-wasi"))
             }
         }
     }
