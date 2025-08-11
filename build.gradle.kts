e43e13b87d725233b0afca28784edd601cc59680
sksamuel <sam@sksamuel.com>
2025-08-11 16:29:41 -0500

    Revert "Reset to native only"
    This reverts commit b73769b6b2fa590d48fa8a46d26f39be5db82d26.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.kotlinMultiplatform) apply false
-//    alias(libs.plugins.androidLibrary) apply false
-//    alias(libs.plugins.kotest)
-//    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotest)
+    alias(libs.plugins.ksp)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,35 +4,35 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-//    alias(libs.plugins.androidLibrary)
-//    alias(libs.plugins.kotest)
-//    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotest)
+    alias(libs.plugins.ksp)
 }
 
 
 // Blocked by KSP.
 ///////// ITS EITHER THIS BLOCK OR DISABLING WAMSWASI //////////////////////////////////////////////////////////////////
-//tasks.configureEach {
-//    val n = name
-//    if (
-//        n.startsWith("compileTestKotlinWasmWasi") || // compilation task
-//        n.endsWith("WasmWasiTest")                   // execution task
-//    ) {
-//        enabled = false
-//    }
-//}
-//configurations
-//    .matching { it.name.endsWith("wasmWasiTestCompileClasspath") || it.name.endsWith("wasmWasiTestRuntimeClasspath") }
-//    .configureEach {
-//        exclude(
-//            group = "io.kotest",
-//            module = "kotest-framework-engine-wasm-wasi"
-//        )
-//        exclude(
-//            group = "io.kotest",
-//            module = "kotest-assertions-core-wasm-wasi"
-//        )
-//    }
+tasks.configureEach {
+    val n = name
+    if (
+        n.startsWith("compileTestKotlinWasmWasi") || // compilation task
+        n.endsWith("WasmWasiTest")                   // execution task
+    ) {
+        enabled = false
+    }
+}
+configurations
+    .matching { it.name.endsWith("wasmWasiTestCompileClasspath") || it.name.endsWith("wasmWasiTestRuntimeClasspath") }
+    .configureEach {
+        exclude(
+            group = "io.kotest",
+            module = "kotest-framework-engine-wasm-wasi"
+        )
+        exclude(
+            group = "io.kotest",
+            module = "kotest-assertions-core-wasm-wasi"
+        )
+    }
 // END BLOCKED BY SKP //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -44,7 +44,7 @@ kotlin {
     }
 
     // for android tests, use the `kotestDebugUnitTest` or `kotestReleaseUnitTest` tasks
-//    androidTarget()
+    androidTarget()
 
     // native has a bug - test reports aren't written at the module level, but they are at the aggregated root level
     // note: to run tests for native targets, use the standard gradle task eg linuxX64Test
@@ -52,64 +52,64 @@ kotlin {
     linuxArm64()
     mingwX64()
 
-//    macosArm64()
+    macosArm64()
     macosX64()
-//    iosX64()
-//    iosArm64()
-//    iosSimulatorArm64()
-//    watchosDeviceArm64()
-//    watchosSimulatorArm64()
-//    watchosX64()
-//    watchosArm32()
-//    watchosArm64()
-//    tvosSimulatorArm64()
-//    tvosX64()
-//    tvosArm64()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
 
     // kotlin has no support for tests for androidNative targets, so they'll be skipped at runtime
     // it will also error saying the compiler is only at 2.0 - I cannot figure out how why since it's set to 2.2
-//    androidNativeX64()
-//    androidNativeX86()
-//    androidNativeArm32()
-//    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+    androidNativeArm32()
+    androidNativeArm64()
 
     // to run JVM tests use either the gradle task `jvmTest` (requires engine dep) or the `jvmKotest` task (enhanced Kotest support)
-//    jvm()
-//
-//    // for js tests run the `jsKotest` task
-//    js {
-//        outputModuleName = "@kotest/test"
-//        version = "1.0.0"
-//        binaries.executable()
-//        nodejs()
-//        browser { testTask { useKarma { useChromeHeadless() } } }
-//    }
-//
-//    // won't do anything but won't fail either
-//    @OptIn(ExperimentalWasmDsl::class)
-//    wasmWasi {
-//        nodejs()
-//    }
-//
-//    // for wasmJs tests run the `wasmJsKotest` task
-//    @OptIn(ExperimentalWasmDsl::class)
-//    wasmJs {
-//        browser {
-//            testTask { useKarma { useChromeHeadless() } }
-//            val rootDirPath = project.rootDir.path
-//            val projectDirPath = project.projectDir.path
-//            commonWebpackConfig {
-//                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-//                    static = (static ?: mutableListOf()).apply {
-//                        // Serve sources to debug inside browser
-//                        add(rootDirPath)
-//                        add(projectDirPath)
-//                    }
-//                }
-//            }
-//        }
-//        nodejs()
-//    }
+    jvm()
+
+    // for js tests run the `jsKotest` task
+    js {
+        outputModuleName = "@kotest/test"
+        version = "1.0.0"
+        binaries.executable()
+        nodejs()
+        browser { testTask { useKarma { useChromeHeadless() } } }
+    }
+
+    // won't do anything but won't fail either
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmWasi {
+        nodejs()
+    }
+
+    // for wasmJs tests run the `wasmJsKotest` task
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            testTask { useKarma { useChromeHeadless() } }
+            val rootDirPath = project.rootDir.path
+            val projectDirPath = project.projectDir.path
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(rootDirPath)
+                        add(projectDirPath)
+                    }
+                }
+            }
+        }
+        nodejs()
+    }
 
     sourceSets {
         commonMain {
@@ -119,40 +119,40 @@ kotlin {
         }
         commonTest {
             dependencies {
-//                implementation(libs.kotest.framework)
-//                implementation(libs.kotest.assert)
+                implementation(libs.kotest.framework)
+                implementation(libs.kotest.assert)
                 implementation(kotlin("test"))
             }
         }
         // these deps should be added by gradle automatically from common main, but they don't seem to always work
-//        jsMain {
-//            dependencies {
-//                api(kotlin("stdlib-js"))
-//            }
-//        }
-//        // these deps should be added by gradle automatically from common main, but they don't seem to always work
-//        wasmJsMain {
-//            dependencies {
-//                api(kotlin("stdlib-wasm-js"))
-//            }
-//        }
-//        // these deps should be added by gradle automatically from common main, but they don't seem to always work
-//        wasmWasiMain {
-//            dependencies {
-//                api(kotlin("stdlib-wasm-wasi"))
-//            }
-//        }
+        jsMain {
+            dependencies {
+                api(kotlin("stdlib-js"))
+            }
+        }
+        // these deps should be added by gradle automatically from common main, but they don't seem to always work
+        wasmJsMain {
+            dependencies {
+                api(kotlin("stdlib-wasm-js"))
+            }
+        }
+        // these deps should be added by gradle automatically from common main, but they don't seem to always work
+        wasmWasiMain {
+            dependencies {
+                api(kotlin("stdlib-wasm-wasi"))
+            }
+        }
     }
 }
 
-//android {
-//    namespace = "io.kotest.test.shared"
-//    compileSdk = libs.versions.android.compileSdk.get().toInt()
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
-//    defaultConfig {
-//        minSdk = libs.versions.android.minSdk.get().toInt()
-//    }
-//}
+android {
+    namespace = "io.kotest.test.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
