19e17056b2cb3b1206e85c24597d496976b7abf9
sksamuel <sam@sksamuel.com>
2025-08-11 21:24:01 -0500

    Update dependencies: remove unused repositories and upgrade Kotest to 6.0.0.M16

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,7 @@ pluginManagement {
             }
         }
         mavenCentral()
-        mavenLocal()
         gradlePluginPortal()
-        maven("https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 
@@ -26,9 +24,7 @@ dependencyResolutionManagement {
                 includeGroupAndSubgroups("com.google")
             }
         }
-        mavenLocal()
         mavenCentral()
-        maven("https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 
